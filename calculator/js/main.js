1f689f226b31a68dcc200ec6761b8e97c9129e26
Ivailo Cvetanov <pivnika_10@abv.bg>
2018-11-02 10:56:34 +0200

    added MAX_VALUE_LENGTH variable

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -2,6 +2,7 @@ const notifications = document.getElementById('notifications');
 const calc = document.getElementById('calculator');
 const buttons = document.getElementById('buttons');
 const screen = document.getElementById('screen');
+const MAX_VALUE_LENGTH = 16;
 const regEx = {
     signs: /[.*/+-]-?/gi,
     regLastSign: /(\d*\.)?\d+$/,
@@ -13,53 +14,52 @@ let zeroFlag = false;
 
 document.body.addEventListener('keydown', function (ev) {
     let screenValue = screen.value;
-    let lastsym = screenValue.substring(screen.value.length , screenValue.length -1);
+    let lastsym = screenValue.substring(screen.value.length, screenValue.length - 1);
 
-    if (pointFlag && ev.key === '.' 
-        || zeroFlag && ev.key === '0'
-        || ev.key === lastsym && ev.key.match(regEx.signs) 
-        || lastsym.match(regEx.signs) && ev.key === '.') {
+    if (pointFlag && ev.key === '.' ||
+        zeroFlag && ev.key === '0' ||
+        ev.key === lastsym && ev.key.match(regEx.signs) ||
+        lastsym.match(regEx.signs) && ev.key === '.') {
         return;
     }
 
     checkSpecialButtons(ev);
 
-    if (screenValue.length >= 16) {
+    if (screenValue.length >= MAX_VALUE_LENGTH) {
         notification("Maximum length value reached.");
         return;
     }
 
     if (lastsym.match(regEx.signs) && ev.key.match(regEx.signs)) {
         backspace();
-    } 
+    }
 
-    if (regEx.insert.test(ev.key) 
-        && !(ev.keyCode >= 112 && ev.keyCode <= 123)) {
+    if (regEx.insert.test(ev.key) &&
+        !(ev.keyCode >= 112 && ev.keyCode <= 123)) {
         insert(ev.key);
-    } 
+    }
     return;
 });
 
 buttons.addEventListener('click', function (ev) {
-    let screenValue = screen.value;
-    let lastsym = screenValue.substring(screenValue.length , screenValue.length -1);
-
     if (ev.target.tagName !== "BUTTON") {
         return;
     }
-
+    
+    let screenValue = screen.value;
+    let lastsym = screenValue.substring(screenValue.length, screenValue.length - 1);
     let digit = ev.target.value;
 
-    if (pointFlag && ev.target.innerText === '.' 
-        || zeroFlag && ev.target.innerText === '0' 
-        || digit === lastsym && digit.match(regEx.signs)
-        || lastsym.match(regEx.signs) && ev.target.innerText === '.') {
+    if (pointFlag && ev.target.innerText === '.' ||
+        zeroFlag && ev.target.innerText === '0' ||
+        digit === lastsym && digit.match(regEx.signs) ||
+        lastsym.match(regEx.signs) && ev.target.innerText === '.') {
         return;
     }
 
     checkSpecialButtons(ev);
 
-    if (screenValue.length >= 16) {
+    if (screenValue.length >= MAX_VALUE_LENGTH) {
         notification("Maximum length value reached.");
         return;
     }
@@ -67,12 +67,12 @@ buttons.addEventListener('click', function (ev) {
     if (digit.match(regEx.signs) && lastsym.match(regEx.signs) && lastsym !== digit) {
         backspace();
     }
-        
+
     insert(digit);
 });
 
 function checkSpecialButtons(ev) {
-    if(ev.key === undefined) {
+    if (ev.key === undefined) {
         ev.key = 'hack';
     }
 
@@ -87,17 +87,17 @@ function checkSpecialButtons(ev) {
         return calculate();
     }
     let screenValue = screen.value;
-    let lastsym = screenValue.substring(screenValue.length , screenValue.length -1);
+    let lastsym = screenValue.substring(screenValue.length, screenValue.length - 1);
 
     if (lastsym === '.') {
         pointFlag = true;
-    } else if ((ev.target.innerText.match(regEx.signs) && ev.type === 'click')
-        || (ev.key.match(regEx.signs) && ev.type === 'keydown')) {
+    } else if ((ev.target.innerText.match(regEx.signs) && ev.type === 'click') ||
+        (ev.key.match(regEx.signs) && ev.type === 'keydown')) {
         pointFlag = false;
     }
-    if((screenValue  === '' && ev.target.innerText === '0' && ev.type === 'click') 
-        || (screenValue  === '' && ev.key === '0' && ev.type === 'keydown')) {
-            zeroFlag = true;
+    if ((screenValue === '' && ev.target.innerText === '0' && ev.type === 'click') ||
+        (screenValue === '' && ev.key === '0' && ev.type === 'keydown')) {
+        zeroFlag = true;
     } else {
         zeroFlag = false;
     }
@@ -112,21 +112,21 @@ function clearScreen() {
 }
 
 function backspace() {
-    let screenValue = screen.value,
-        lastsym = screenValue.substring(screenValue.length , screenValue.length -1);
+    let screenValue = screen.value;
+    let lastsym = screenValue.substring(screenValue.length, screenValue.length - 1);
 
     if (lastsym.match(regEx.signs)) {
-        let strval = screen.value.substring(0, screen.value.length -1);
+        let strval = screen.value.substring(0, screen.value.length - 1);
         let lastIndexSign = strval.search(regEx.regLastSign);
 
         res = strval.substr(lastIndexSign);
 
-        if(res.includes('.')) {
+        if (res.includes('.')) {
             pointFlag = true;
         } else {
             pointFlag = false;
         }
-    }  
+    }
     if (lastsym === '.') {
         pointFlag = false;
     }
@@ -135,7 +135,7 @@ function backspace() {
 
 function calculate() {
     let screenValue = screen.value;
-    let lastsym = screenValue.substring(screenValue.length , screenValue.length -1);
+    let lastsym = screenValue.substring(screenValue.length, screenValue.length - 1);
 
     if (screenValue !== '') {
         if (lastsym.match(regEx.signs)) {
@@ -148,7 +148,7 @@ function calculate() {
 
             screen.value = Math.round(eval(screenValue) * 100) / 100;
 
-            if(screen.value.includes('.')) {
+            if (screen.value.includes('.')) {
                 pointFlag = true;
             } else {
                 pointFlag = false;
