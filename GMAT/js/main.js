699bbea2ffddb5b7ba76645fd46d73f0812b99a2
Ivailo Cvetanov <pivnika_10@abv.bg>
2018-11-05 11:16:31 +0200

    add first design, local storage data save

--- GMAT/js/main.js
+++ GMAT/js/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form');
+const people = document.getElementById('peopleDiv');
 const nameField = document.getElementById('name');
 const addressField = document.getElementById('address');
 const emailField = document.getElementById('email');
@@ -9,15 +10,45 @@ const regEx = {
     phoneField: /^\+?\d+(-\d+)*$/g,
     websiteField: /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/g
 };
+let saveData = JSON.parse(localStorage.saveData || null) || {};
+
+//let QUERY = addressField.value;
 
 window.addEventListener('DOMContentLoaded', function() {
     nameField.focus();
 });
 
+addressField.style = '';
 
 function validateForm() {
-    alert(1);
+
+    let newPerson = {
+        name: nameField.value,
+        email: emailField.value,
+        phone: phoneField.value,
+        website: websiteField.value,
+        address: addressField.value
+    }
+
+    function saveStuff(obj) {
+
+        saveData.obj = obj;
+        saveData.time = new Date().getTime();
+        localStorage.saveData = JSON.stringify(saveData);
+    }
+
+    if (saveData.time) alert("You were here: " + saveData.time);
+
+    saveStuff(newPerson);
+    return false;
 }
+
+function clearLocalStorage() {
+    localStorage.clear();
+}
+
+people.innerHTML += saveData.obj.name || "No stored data in local storage!";
+console.log(saveData.obj);
 
 
 
