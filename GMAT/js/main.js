42aa7600654dedb339549d7ae857eb732a8351a3
Ivailo Cvetanov <pivnika_10@abv.bg>
2018-11-05 18:04:37 +0200

    add check for existing email, add regex validation patterns

--- GMAT/js/main.js
+++ GMAT/js/main.js
@@ -1,36 +1,27 @@
 const form = document.getElementById('form');
-const people = document.getElementById('peopleDiv');
 const nameField = document.getElementById('name');
 const addressField = document.getElementById('address');
 const emailField = document.getElementById('email');
 const phoneField = document.getElementById('phone');
 const websiteField = document.getElementById('website');
-const regEx = {
+/*const regEx = {
     nameField: /[A-Za-z]/g,
     phoneField: /^\+?\d+(-\d+)*$/g,
     websiteField: /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/g
-};
-
-let peopleArray = localStorage.getItem('people') ? JSON.parse(localStorage.getItem('people')) : [];
-//let QUERY = addressField.value;
+};*/
+let emails = [];
+for (var i = 0; i < localStorage.length+1; i++) {
+    peopleLS = JSON.parse(localStorage.getItem(i));
+    if(i>0) {
+        emails.push(peopleLS.email);
+    }
+}
 
 window.addEventListener('DOMContentLoaded', function() {
     nameField.focus();
 });
 
-
-Storage.prototype.insertPerson = function(person) {
-    peopleArray.push(person);
-    localStorage.setItem('people', JSON.stringify(peopleArray));
-}
-
-/*
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-});*/
-
-function validateForm() {
+form.addEventListener('submit', function () {
     let newPerson = {
         name: nameField.value,
         email: emailField.value,
@@ -38,35 +29,17 @@ function validateForm() {
         website: websiteField.value,
         address: addressField.value
     }
-  
-    localStorage.insertPerson(newPerson);
-
-    /*function saveStuff(obj) {
-        saveData.obj = obj;
-        saveData.time = new Date().getTime();
-        localStorage.saveData = JSON.stringify(saveData);
+    
+    if(emails.includes(newPerson.email)) {
+        alert('the email exists');
+        return false;
+    } else {
+        let ID = localStorage.length + 1;
+        localStorage.insertPerson(ID, newPerson);
+        form.submit();
     }
-    saveStuff(newPerson);*/
+});
 
-   // localStorage.setObject(newPerson.name, newPerson);
-    alert('data saved!');
-    return false;
+Storage.prototype.insertPerson = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
 }
-
-function clearLocalStorage() {
-    localStorage.clear();
-}
-
-people.innerHTML += localStorage || "No stored data in local storage!";
-
-console.log(localStorage);
-
-for (let i = 0; i < localStorage.length; i++) {
-    let td = document.createElement(td);
-    //td.innerHTML = localStorage.people[]
-}
-//console.log(localStorage.getObject());
-
-
-
-
